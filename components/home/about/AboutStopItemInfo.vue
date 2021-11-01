b5d0af1c6c1088a457c5ac81fbfc9f106876d9d1
peregrin-tuk <eric.thalhammer@gmx.net>
2021-11-01 20:21:01 +0100

    fix color of metro map markers
    + add link to numerous side projects info

--- components/home/about/AboutStopItemInfo.vue
+++ components/home/about/AboutStopItemInfo.vue
@@ -3,11 +3,13 @@
         <div class="text-textBright tracking-wide font-secondary flex flex-col flex-grow">
             <div class="text-textSubtle text-xxs" :class=" { '-ml-4': scanted }">{{ text1 }}</div>
             <div class="text-md">{{ title }}</div>
-            <div class="text-xs" :class="scanted ? 'ml-4' : 'ml-5'">{{ text2 }}</div>        
+            <div class="text-xs" :class="scanted ? 'ml-4' : 'ml-5'"><NuxtLink :to="link">{{ text2 }}</NuxtLink></div>        
         </div>
-        <svg :class="'text-' + color +' stroke-current mt-7 flex-shrink-0'" width="26" height="6" viewBox="0 0 26 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 3L23 3" stroke-width="6" stroke-linecap="round"/>
-        </svg>
+        <div :class="'text-' + color">
+            <svg class="stroke-current mt-7 flex-shrink-0" width="26" height="6" viewBox="0 0 26 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 3L23 3" stroke-width="6" stroke-linecap="round"/>
+            </svg>
+        </div>
     </div>
 </template>
 
@@ -31,6 +33,10 @@ export default {
             required: true,
         },
         text2: {
+            type: String,
+            required: true,
+        },
+        link: {
             type: String,
             required: true,
         },
